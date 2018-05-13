b4221bad35b3214897b84e22117c1346a23022c9
Zach Newton <zach.newton@student.uts.edu.au>
2018-05-13 20:16:54 +1000

    Initial Commit

---
+++ Folder_Hash.ps1
@@ -0,0 +1,7 @@
+﻿Function Get-FolderHash{
+$folder = Read-Host -Prompt 'Enter target folder'
+Get-FileHash -Algorithm SHA256 -Path (Get-ChildItem "$folder\*.*" -Recurse) |
+Select-Object Path, Hash |
+Export-Csv -Path "File_hashes.csv"
+"Output file has been written to the current directory"
+}
